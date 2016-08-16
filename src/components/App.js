fcf38bc6504d31930b035c88602be0a206f7268f
Jackson Cafazzo <doctorjuno@gmail.com>
2016-08-16 15:16:07 -0700

    initial commit with one component

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,31 @@
 import React, { Component } from 'react';
 import Jackson from './Jackson';
-import firebase from 'firebase';
+
+const API_KEY = 'AIzaSyCxj9IPD23Wsa9XRlx5z3z_qGkefRSDy1Q';
+
+// import firebase from 'firebase';
 
   // Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyCxj9IPD23Wsa9XRlx5z3z_qGkefRSDy1Q',
-  authDomain: 'react-tarot.firebaseapp.com',
-  databaseURL: 'https://react-tarot.firebaseio.com',
-  storageBucket: 'react-tarot.appspot.com',
-};
-const firebaseApp = firebase.initializeApp(config);
-const storage = firebaseApp.storage();
-const storageRef = storage.ref();
-const sunRef = storageRef.child('Sun_19.jpg');
+// const config = {
+//   apiKey: 'AIzaSyDBiUc_UZOeQJMmmfuRgucKTX_VdGR5yqM',
+//   authDomain: 'fir-storage-sample-eff34.firebaseapp.com',
+//   databaseURL: 'https://fir-storage-sample-eff34.firebaseio.com',
+//   storageBucket: 'fir-storage-sample-eff34.appspot.com',
+// };
+// const firebaseApp = firebase.initializeApp(config);
+// const storage = firebaseApp.storage();
+// const storageRef = storage.ref();
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
 
-  getCardUrl() {
-    sunRef.getDownloadURL().then(function(url) {
-      this.state.path = url;
-    }).catch(function(error) {
-    //handle the errors
-      console.log('ERROR!!' + error);
-    });
-  }
-
-  drawCard() {
-    return Math.floor(Math.random() * 22);
-  }
-
-  getNewCard(e) {
-    e.preventDefault();
-    this.setState({ card: this.drawCard() });
-  }
-
   render() {
     return (
       <div className='container'>
-        <h1>Hello, world!</h1>
-        <h3>{this.state.card}</h3>
-        <button onClick={e => {
-          this.setState({ card: this.drawCard() });
-        }}>DRAW ANOTHER CARD</button>
-          <p>Let's get funky.
-          { this.getCardUrl() }
-          { this.state.path }
-          </p>
-          <Jackson />
+       <p>haaaaaay</p>
+       <Jackson />
       </div>
     );
   }
--- src/components/Jackson.js
+++ src/components/Jackson.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 
 export default class Jackson extends Component {
-  constructor(props) {
-    super(props);
+
+  constructor() {
+    super();
     this.state = {
       addedValue: '',
       num1: 1,
-      num2: 2
+      num2: 2,
+      magic: 0,
+      numbers: []
     };
   }
   render() {
     return (
     <div>
       <p>Jackson is #{this.state.num1}</p>
+      {this.state.numbers}
+      <button onClick={e => {
+        this.setState({ magic: this.getRandomNumber() });
+      }}>add random number</button>
     </div>
-  );
+    );
+  }
+
+  getRandomNumber() {
+    let numbers = this.state.numbers;
+    let roll = Math.floor(Math.random() * 10);
+    numbers.push(roll);
+    this.setState({ numbers: numbers });
+    return roll;
   }
 }
